8956482c0d39e73fc5c39679c610ea5ffcc011a9
Silke Pilon <silkepilon2009@gmail.com>
2024-08-24 16:42:31 +0200

    init

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,297 @@
+// @ts-nocheck
+"use client";
+import { Metadata } from "next";
+import Image from "next/image";
+import { CounterClockwiseClockIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@/components/ui/button";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Textarea } from "@/components/ui/textarea";
+import { useState, useEffect } from "react";
+import { CodeViewer } from "@/components/code-viewer";
+import { MaxLengthSelector } from "@/components/maxlength-selector";
+import { ModelSelector } from "@/components/model-selector";
+import { PresetActions } from "@/components/preset-actions";
+import { PresetSave } from "@/components/preset-save";
+import { PresetSelector } from "@/components/preset-selector";
+import { PresetShare } from "@/components/preset-share";
+import { TemperatureSelector } from "@/components/temperature-selector";
+import { TopPSelector } from "@/components/top-p-selector";
+import { models, types } from "./data/models";
+import { presets } from "./data/presets";
+import { ModeToggle } from "@/components/mode-toggle";
+import { MapContainer, Marker, TileLayer, useMapEvents } from "react-leaflet";
+import { LayersControl, LayerGroup, Popup } from "react-leaflet";
+import JotiHuntMap from "@/components/map";
+import "leaflet/dist/leaflet.css";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export default function PlaygroundPage() {
+  const THUNDERFOREST_API_KEY = "130d9bc1e10a476c9db594341f4d5579";
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    52.2129919, 5.2793703,
+  ]);
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
+    0, 0,
+  ]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setInitialPosition([latitude, longitude]);
+    });
+  }, []);
+
+  const MapEvents = () => {
+    useMapEvents({
+      click(e) {
+        setSelectedPosition([e.latlng.lat, e.latlng.lng]);
+      },
+    });
+    return null;
+  };
+
+  const Markers = () => {
+    const map = useMapEvents({
+      click(e) {
+        setSelectedPosition([e.latlng.lat, e.latlng.lng]);
+      },
+    });
+
+    return selectedPosition ? (
+      <Marker
+        key={selectedPosition[0]}
+        position={selectedPosition}
+        interactive={true}
+      ></Marker>
+    ) : null;
+  };
+  return (
+    <>
+      <div className="h-full flex-col md:flex">
+        <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+          <h2 style={{ width: "100%" }} className="text-lg font-semibold">
+            JotiHunt Map
+          </h2>
+          <ModeToggle></ModeToggle>
+          <div className="ml-auto flex w-full space-x-2 sm:justify-end">
+            <PresetSelector presets={presets} />
+            <PresetSave />
+            <div className="hidden space-x-2 md:flex">
+              <CodeViewer />
+              <PresetShare />
+            </div>
+            <PresetActions />
+          </div>
+        </div>
+        <Separator />
+        <Tabs defaultValue="complete" className="flex-1">
+          <div className="container h-full py-6">
+            <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px]">
+              <div className="hidden flex-col space-y-4 sm:flex md:order-2">
+                <div className="grid gap-2">
+                  <HoverCard openDelay={200}>
+                    <HoverCardTrigger asChild>
+                      <span className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                        Mode
+                      </span>
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-[320px] text-sm" side="left">
+                      Choose the interface that best suits your task. You can
+                      provide: a simple prompt to complete, starting and ending
+                      text to insert a completion within, or some text with
+                      instructions to edit it.
+                    </HoverCardContent>
+                  </HoverCard>
+                  <TabsList className="grid grid-cols-3">
+                    <TabsTrigger value="complete">
+                      <span className="sr-only">Map</span>
+                      <svg
+                        width="15"
+                        height="15"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M1.20308 1.04312C1.00481 0.954998 0.772341 1.0048 0.627577 1.16641C0.482813 1.32802 0.458794 1.56455 0.568117 1.75196L3.92115 7.50002L0.568117 13.2481C0.458794 13.4355 0.482813 13.672 0.627577 13.8336C0.772341 13.9952 1.00481 14.045 1.20308 13.9569L14.7031 7.95693C14.8836 7.87668 15 7.69762 15 7.50002C15 7.30243 14.8836 7.12337 14.7031 7.04312L1.20308 1.04312ZM4.84553 7.10002L2.21234 2.586L13.2689 7.50002L2.21234 12.414L4.84552 7.90002H9C9.22092 7.90002 9.4 7.72094 9.4 7.50002C9.4 7.27911 9.22092 7.10002 9 7.10002H4.84553Z"
+                          fill="currentColor"
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </TabsTrigger>
+                    <TabsTrigger value="insert">
+                      <span className="sr-only">Location</span>
+                      <svg
+                        width="15"
+                        height="15"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M10.3285 1.13607C10.1332 0.940809 9.81662 0.940808 9.62136 1.13607C9.42609 1.33133 9.42609 1.64792 9.62136 1.84318L10.2744 2.49619L5.42563 6.13274L4.31805 5.02516C4.12279 4.8299 3.80621 4.8299 3.61095 5.02516C3.41569 5.22042 3.41569 5.537 3.61095 5.73226L5.02516 7.14648L6.08582 8.20714L2.81545 11.4775C2.62019 11.6728 2.62019 11.9894 2.81545 12.1846C3.01072 12.3799 3.3273 12.3799 3.52256 12.1846L6.79293 8.91425L7.85359 9.97491L9.2678 11.3891C9.46306 11.5844 9.77965 11.5844 9.97491 11.3891C10.1702 11.1939 10.1702 10.8773 9.97491 10.682L8.86733 9.57443L12.5039 4.7257L13.1569 5.37871C13.3522 5.57397 13.6687 5.57397 13.864 5.37871C14.0593 5.18345 14.0593 4.86687 13.864 4.6716L12.8033 3.61094L11.3891 2.19673L10.3285 1.13607ZM6.13992 6.84702L10.9887 3.21047L11.7896 4.01142L8.15305 8.86015L6.13992 6.84702Z"
+                          fill="currentColor"
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </TabsTrigger>
+                    <TabsTrigger value="edit">
+                      <span className="sr-only">Chat</span>
+                      <svg
+                        width="15"
+                        height="15"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M12.5 3L2.5 3.00002C1.67157 3.00002 1 3.6716 1 4.50002V9.50003C1 10.3285 1.67157 11 2.5 11H7.50003C7.63264 11 7.75982 11.0527 7.85358 11.1465L10 13.2929V11.5C10 11.2239 10.2239 11 10.5 11H12.5C13.3284 11 14 10.3285 14 9.50003V4.5C14 3.67157 13.3284 3 12.5 3ZM2.49999 2.00002L12.5 2C13.8807 2 15 3.11929 15 4.5V9.50003C15 10.8807 13.8807 12 12.5 12H11V14.5C11 14.7022 10.8782 14.8845 10.6913 14.9619C10.5045 15.0393 10.2894 14.9965 10.1464 14.8536L7.29292 12H2.5C1.11929 12 0 10.8807 0 9.50003V4.50002C0 3.11931 1.11928 2.00003 2.49999 2.00002Z"
+                          fill="currentColor"
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </TabsTrigger>
+                  </TabsList>
+                </div>
+                <ModelSelector types={types} models={models} />
+                <TemperatureSelector defaultValue={[0.56]} />
+                <MaxLengthSelector defaultValue={[256]} />
+                <TopPSelector defaultValue={[0.9]} />
+              </div>
+              <div className="md:order-1">
+                <TabsContent value="complete" className="mt-0 border-0 p-0">
+                  <div className="flex h-full flex-col space-y-4">
+                    {/* <Textarea placeholder="Write a tagline for an ice cream shop" /> */}
+                    {/*  // @ts-ignore */}
+                    {/* <MapContainer
+                      viewport={{ center: [40.505, -100.09], zoom: 13 }}
+                    >
+                      <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        className="min-h-[400px] flex-1 p-4 md:min-h-[700px] lg:min-h-[700px]"
+                      />
+                    </MapContainer> */}
+                    {/* <MapContainer
+                      center={initialPosition}
+                      zoom={10}
+                      className="min-h-[400px] flex-1 p-4 md:min-h-[700px] lg:min-h-[700px]"
+                      style={{ borderRadius: "0.75rem" }}
+                    >
+                      <Markers />
+                      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    </MapContainer> */}
+                    <JotiHuntMap />
+                    <div className="flex items-center space-x-2">
+                      <Button>Submit</Button>
+                      <Button variant="secondary">
+                        <span className="sr-only">Show history</span>
+                        <CounterClockwiseClockIcon className="h-4 w-4" />
+                      </Button>
+                    </div>
+                  </div>
+                </TabsContent>
+                <TabsContent value="insert" className="mt-0 border-0 p-0">
+                  <div className="flex flex-col space-y-4">
+                    <div className="grid h-full grid-rows-2 gap-6 lg:grid-cols-2 lg:grid-rows-1">
+                      {/* <Textarea
+                        placeholder="We're writing to [inset]. Congrats from OpenAI!"
+                        className="h-full min-h-[300px] lg:min-h-[700px] xl:min-h-[700px]"
+                      /> */}
+                      <MapContainer
+                        center={initialPosition}
+                        zoom={13}
+                        className=""
+                        style={{ borderRadius: "0.75rem", height: "80vh" }}
+                      >
+                        <Markers />
+                        <LayersControl position="topright">
+                          <LayersControl.BaseLayer checked name="OpenStreetMap">
+                            <TileLayer
+                              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            />
+                          </LayersControl.BaseLayer>
+                          <LayersControl.Overlay name="Bike Lanes">
+                            <TileLayer
+                              url={`https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=${THUNDERFOREST_API_KEY}`}
+                              attribution='&copy; <a href="https://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            />
+                          </LayersControl.Overlay>
+                          <LayersControl.Overlay name="Walking Paths">
+                            <TileLayer
+                              url={`https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=${THUNDERFOREST_API_KEY}`}
+                              attribution='&copy; <a href="https://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            />
+                          </LayersControl.Overlay>
+                        </LayersControl>
+                        <MapEvents />
+                        {selectedPosition && (
+                          <Marker position={selectedPosition} />
+                        )}
+                      </MapContainer>
+                      <div className="rounded-md border bg-muted"></div>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <Button>Submit</Button>
+                      <Button variant="secondary">
+                        <span className="sr-only">Show history</span>
+                        <CounterClockwiseClockIcon className="h-4 w-4" />
+                      </Button>
+                    </div>
+                  </div>
+                </TabsContent>
+                <TabsContent value="edit" className="mt-0 border-0 p-0">
+                  <div className="flex flex-col space-y-4">
+                    <div className="grid h-full gap-6 lg:grid-cols-2">
+                      <div className="flex flex-col space-y-4">
+                        <div className="flex flex-1 flex-col space-y-2">
+                          <Label htmlFor="input">Input</Label>
+                          <Textarea
+                            id="input"
+                            placeholder="We is going to the market."
+                            className="flex-1 lg:min-h-[580px]"
+                          />
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                          <Label htmlFor="instructions">Instructions</Label>
+                          <Textarea
+                            id="instructions"
+                            placeholder="Fix the grammar."
+                          />
+                        </div>
+                      </div>
+                      <div className="mt-[21px] min-h-[400px] rounded-md border bg-muted lg:min-h-[700px]" />
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <Button>Submit</Button>
+                      <Button variant="secondary">
+                        <span className="sr-only">Show history</span>
+                        <CounterClockwiseClockIcon className="h-4 w-4" />
+                      </Button>
+                    </div>
+                  </div>
+                </TabsContent>
+              </div>
+            </div>
+          </div>
+        </Tabs>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/components/map.tsx
@@ -0,0 +1,120 @@
+// @ts-nocheck
+import React, { useState, useEffect } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  LayersControl,
+  Circle,
+  Popup,
+} from "react-leaflet";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+
+const JotiHuntMap = () => {
+  const [groups, setGroups] = useState([]);
+  const [selectedGroup, setSelectedGroup] = useState(null);
+  const centerOfNetherlands = [52.2858356, 5.6549385899207];
+
+  useEffect(() => {
+    fetch("https://jotihunt.nl/api/2.0/subscriptions")
+      .then((response) => response.json())
+      .then((data) => setGroups(data.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const THUNDERFOREST_API_KEY = "130d9bc1e10a476c9db594341f4d5579";
+
+  return (
+    <MapContainer
+      center={centerOfNetherlands}
+      zoom={10}
+      style={{ height: "80vh", width: "100%", borderRadius: "0.75rem" }}
+    >
+      <LayersControl position="topright">
+        <LayersControl.BaseLayer checked name="OpenStreetMap">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.Overlay name="Bike Lanes">
+          <TileLayer
+            url={`https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=${THUNDERFOREST_API_KEY}`}
+            attribution='&copy; <a href="https://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="Walking Paths">
+          <TileLayer
+            url={`https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=${THUNDERFOREST_API_KEY}`}
+            attribution='&copy; <a href="https://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </LayersControl.Overlay>
+      </LayersControl>
+
+      {groups.map((group, index) => (
+        <Circle
+          key={index}
+          center={[parseFloat(group.lat), parseFloat(group.long)]}
+          radius={1000}
+          pathOptions={{ fillColor: getRandomColor(), color: "black" }}
+          eventHandlers={{
+            click: () => setSelectedGroup(group),
+          }}
+        >
+          <Popup className="request-popup">
+            {/* <Card>
+              <CardHeader>{group.name}</CardHeader>
+              <CardContent>
+                <p>Accommodatie: {group.accomodation}</p>
+                <p>
+                  Adres: {group.street} {group.housenumber}
+                  {group.housenumber_addition}
+                </p>
+                <p>
+                  {group.postcode} {group.city}
+                </p>
+                <Button onClick={() => setSelectedGroup(null)}>Sluiten</Button>
+              </CardContent>
+            </Card> */}
+            <p>Accommodatie: {group.accomodation}</p>
+            <p>
+              Adres: {group.street} {group.housenumber}
+              {group.housenumber_addition}
+            </p>
+            <p>
+              {group.postcode} {group.city}
+            </p>
+          </Popup>
+        </Circle>
+      ))}
+
+      {selectedGroup && (
+        <Card className="absolute top-4 right-4 z-[1000] w-64">
+          <CardHeader>{selectedGroup.name}</CardHeader>
+          <CardContent>
+            <p>Accommodatie: {selectedGroup.accomodation}</p>
+            <p>
+              Adres: {selectedGroup.street} {selectedGroup.housenumber}
+              {selectedGroup.housenumber_addition}
+            </p>
+            <p>
+              {selectedGroup.postcode} {selectedGroup.city}
+            </p>
+            <Button onClick={() => setSelectedGroup(null)}>Sluiten</Button>
+          </CardContent>
+        </Card>
+      )}
+    </MapContainer>
+  );
+};
+
+export default JotiHuntMap;
